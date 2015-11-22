f286de3ee22deeae77b94002b939dcc6a296c874
tonirilix <jesusmontes.mx@hotmail.com>
2015-11-22 12:47:51 -0600

    Starting with 'Programming in scala' book

--- 01_helloworld/src/ScalaApp.scala
+++ 01_helloworld/src/ScalaApp.scala
@@ -4,5 +4,16 @@
 object ScalaApp {
   def main(args: Array[String]) {
     println("Hello, world!");
+
+
+    /**
+      * Some codes examples in the introduction of Programming in scala
+      */
+
+    var capital = Map("US" -> "Washington", "France" -> "Paris");
+
+    capital += ("Japan" -> "Tokyo");
+
+    println(capital("France"));
   }
 }
