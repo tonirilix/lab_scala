2ce15cf3a7afb8445c4a7f36ffbc23105fab40a7
tonirilix <jesusmontes.mx@hotmail.com>
2015-12-13 12:38:51 -0600

    Understanding how to declare and assign vars and typed vars

---
+++ 02_FirstSteps/src/ScalaShell.scala
@@ -0,0 +1,34 @@
+/**
+  * Created by tonirilix on 12/13/15.
+  */
+object ScalaApp {
+
+
+  /**
+    * Main!
+    * @param args
+    */
+  def main(args: Array[String]) {
+    println("Hello, world!");
+  }
+
+
+  /**
+    * Define some variables.
+    * The next following are examples of "type inference" because
+    * we don't need to define the type because is inferred by the assigned value type
+    */
+  // Val is similar to a final variable in Java, we can't reassign it
+  val msg = "Hello val!"
+  // Var is a non-final variable so it can be reassigned
+  var msg2 = "Hello var"
+
+  /**
+    * However we can define a type if needed as follows
+    */
+  var msg3: String = "A typed var"
+  var msg4: java.lang.String = "A typed var in a long way"
+  println(msg3)
+  println(msg4)
+
+}
