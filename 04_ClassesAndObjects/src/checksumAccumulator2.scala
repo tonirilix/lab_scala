de8a76eaa561ab7282fa31218d17d1f651fae53d
tonirilix <jesusmontes.mx@hotmail.com>
2016-01-09 12:02:32 -0600

    Leave off curly braces when if a method computes only a single result expression

--- 04_ClassesAndObjects/src/checksumAccumulator2.scala
+++ 04_ClassesAndObjects/src/checksumAccumulator2.scala
@@ -1,6 +1,16 @@
 /**
   * Created by tonirilix on 1/9/16.
   */
-class checksumAccumulator2 {
 
+/**
+  * NOTES:
+  * 1. You can leave off the curly braces if a method computes only a single result expression.
+  * 2.
+  */
+
+class ChecksumAccumulator2 {
+  private var sum = 0;
+
+  def add(b: Byte): Unit = sum += b;
+  def checksum(): Int = ~(sum & 0xFF) + 1;
 }
