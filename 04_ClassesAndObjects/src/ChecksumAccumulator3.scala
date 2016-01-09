189cef894cc7fb38e87c56936abc5ffcf7ef9f98
tonirilix <jesusmontes.mx@hotmail.com>
2016-01-09 12:03:21 -0600

    We can leave off the result type if the method has side effects and the result type is Unit

--- 04_ClassesAndObjects/src/ChecksumAccumulator3.scala
+++ 04_ClassesAndObjects/src/ChecksumAccumulator3.scala
@@ -1,6 +1,20 @@
 /**
   * Created by tonirilix on 1/9/16.
   */
-class ChecksumAccumulator3 {
 
+/**
+  * NOTES:
+  * 1. Methods with a result type Unit are executed for their side effects.
+  *
+  * 2. Another way to express side effects methods is to leave off the result type and the equals sign,
+  * and enclose the body of the method in curly braces
+  *
+  * 3. Whenever you leave off the equals sign before the body of a functoin, its result type will
+  * definitely be Unit
+  */
+class ChecksumAccumulator3 {
+  private var sum = 0;
+
+  def add(b: Byte) { sum += b }
+  def checksum(): Int = { ~(sum & 0xFF) + 1 }
 }
