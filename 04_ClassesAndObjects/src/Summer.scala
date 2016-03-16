3cd80762bdf56dff676756be89d1d621cf5925d6
tonirilix <jesusmontes.mx@hotmail.com>
2016-03-15 19:27:05 -0600

    Adding notes

--- 04_ClassesAndObjects/src/Summer.scala
+++ 04_ClassesAndObjects/src/Summer.scala
@@ -1,6 +1,13 @@
 /**
   * Created by tonirilix on 3/15/16.
   */
+
+/**
+  * NOTES:
+  * 1. A singleton object that does not share the same name with a companion class
+  *   is called a standalone object
+  */
+
 import ChecksumAccumulator.calculate
 
 object Summer{
